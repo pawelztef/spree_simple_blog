70ce5e747a3209cff029c8eb8a6b26e89e2b88b9
pawelztef <pawelztef@gmail.com>
2018-01-29 14:21:00 +0000

    controllers customized to handle mobility flashes added

--- app/controllers/spree/admin/blog_entries_controller.rb
+++ app/controllers/spree/admin/blog_entries_controller.rb
@@ -1,20 +1,36 @@
 class Spree::Admin::BlogEntriesController < Spree::Admin::ResourceController
   helper 'spree/blog_entries'
+  before_action :set_mobility
 
   def new
     Spree::BlogEntry.new
   end
 
   def edit
+    @blog_entry = Spree::BlogEntry.find(params[:id])
   end
 
   def update
 
+    @blog_entry = Spree::BlogEntry.find(params[:id])
+    if @blog_entry.update(entry_params)
+      flash[:success] = flash_message_for(@blog_entry, :successfully_updated)
+      redirect_to admin_blog_entries_url
+    else
+      render :new
+    end
+  end
+
+  def mobility
+    session[:mobility_locale] = params[:mobility_locale]
+
+    redirect_to admin_blog_entries_url 
   end
 
   def create
     @entry = Spree::BlogEntry.new(entry_params)
     if @entry.save
+      flash[:success] = flash_message_for(@blog_entry, :successfully_created)
       redirect_to admin_blog_entries_url
     else
       render :new
@@ -37,4 +53,9 @@ class Spree::Admin::BlogEntriesController < Spree::Admin::ResourceController
     params.require(:blog_entry).permit!
   end
 
+  def set_mobility
+    if session[:mobility_locale]
+      Mobility.locale = session[:mobility_locale]
+    end
+  end
 end
