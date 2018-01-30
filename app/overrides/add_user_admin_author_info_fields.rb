cd6e1b8b24d4a67ee61eacc6db694475cb28cb48
pawelztef <pawelztef@gmail.com>
2018-01-30 11:59:25 +0000

    styles and overrides of user fields

--- app/overrides/add_user_admin_author_info_fields.rb
+++ app/overrides/add_user_admin_author_info_fields.rb
@@ -3,3 +3,15 @@ Deface::Override.new(:virtual_path => "spree/admin/users/_form",
                      :insert_bottom => "[data-hook='admin_user_form_fields']",
                      :partial => "spree/admin/users/author_info_fields",
                      :disabled => false)
+
+Deface::Override.new(:virtual_path => "spree/admin/users/_form",
+                     :name => "style_author_fileds",
+                     :replace => "strong",
+                     :text => "<label class='roles-label'>Roles</label>",
+                     :disabled => false)
+
+Deface::Override.new(:virtual_path => "spree/admin/users/_form",
+                     :name => "style_role_checkboxs",
+                     :set_attributes => "div.checkbox",
+                     :attributes => {:class => 'checkbox-role'},
+                     :disabled => false)
